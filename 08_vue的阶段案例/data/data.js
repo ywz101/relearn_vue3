0acc0d4a7086d23bc406ad586def77d0f198306a
ywz101 <ywz101@gmail.com>
2023-02-18 16:30:24 +0800

    08-vue的阶段案例

---
+++ 08_vue的阶段案例/data/data.js
@@ -0,0 +1,30 @@
+const books = [
+  {
+    id: 1,
+    name: '《书籍1》',
+    date: '2006-9',
+    price: 85,
+    count: 1
+  },
+  {
+    id: 2,
+    name: '《书籍2》',
+    date: '2006-2',
+    price: 59,
+    count: 1
+  },
+  {
+    id: 3,
+    name: '《书籍3》',
+    date: '2008-10',
+    price: 39,
+    count: 1
+  },
+  {
+    id: 4,
+    name: '《书籍4》',
+    date: '2006-3',
+    price: 128,
+    count: 1
+  }
+]
