592d20a5a12135d3d24a33e835ec68b03f47db2b
ywz101 <ywz101@gmail.com>
2023-02-19 18:28:16 +0800

    vue-cli

---
+++ 02_learn_vue_component/01_product_demo/src/App.vue
@@ -0,0 +1,29 @@
+<script>
+  import Counter from './Counter.vue'
+
+  export default {
+    // 注册局部组件
+    components: {
+      Counter
+    },
+    data() {
+      return {
+        message: 'Hello Vue CLI'
+      }
+    }
+  }
+</script>
+
+<template>
+  <h2>{{ message }}</h2>
+
+  <hr>
+
+  <!-- 使用组件 -->
+  <!-- 建议使用大驼峰的写法: 聚焦我们的视野, 一目也然的就知道此处是组件 -->
+  <Counter></Counter>
+</template>
+
+<style>
+
+</style>
---
+++ 02_learn_vue_component/01_product_demo/src/Counter.vue
@@ -0,0 +1,27 @@
+<script>
+  export default {
+    data() {
+      return {
+        counter: 0
+      }
+    },
+    methods: {
+      increment() {
+        this.counter++
+      },
+      decrement() {
+        this.counter--
+      }
+    }
+  }
+</script>
+
+<template>
+  <h2>当前计数: {{ counter }}</h2>
+  <button @click="increment">+1</button>
+  <button @click="decrement">-1</button>
+</template>
+
+<style>
+
+</style>
